fa5c515098114707ab05eebfd7f5162455e4cbfb
LillyKate <>
2021-10-10 09:30:14 +0200

    added general cross_val_test for classifier comparison

--- ml.py
+++ ml.py
@@ -8,6 +8,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
 
+import pandas as pd
+
 # settings for multiclass problems?
 
 def cross_validate(df, k):
@@ -31,7 +33,7 @@ def cross_validate(df, k):
 def cross_val_mat_SVM(df, k):
     results = {}
     pipeline = Pipeline([
-        ('vect', CountVectorizer()),
+        ('vect', CountVectorizer(ngram_range=(2,3))),
         ('tfidf', TfidfTransformer()),
         ('clf', LinearSVC())
     ])
@@ -45,10 +47,10 @@ def cross_val_mat_SVM(df, k):
     
     return results
 
-def cross_val_dtree(df, k, list):
+def cross_val_dtree(df, k, list): # list as possibility to include stopwords
     results = {}
     pipeline = Pipeline([
-        ('tfidf_vec', TfidfVectorizer()),
+        ('tfidf_vec', TfidfVectorizer(ngram_range=(2,3))),
         ('selector',SelectKBest(k=50)),
         #('tfidf_vec', TfidfVectorizer(stop_words=list,max_features=20)),
         ('tree_clf', DecisionTreeClassifier(criterion='gini')),
@@ -66,7 +68,7 @@ def cross_val_dtree(df, k, list):
 def cross_val_reg(df, k):
     results = {}
     pipeline = Pipeline([
-        ('tfidf_vec', TfidfVectorizer()),
+        ('tfidf_vec', TfidfVectorizer(ngram_range=(2,3))),
         ('selector',SelectKBest(k=50)),
         ('reg_clf', LogisticRegression(solver='lbfgs')),
     ])
@@ -78,6 +80,22 @@ def cross_val_reg(df, k):
     results['y_pred'] = cross_val_predict(pipeline, x, y, cv = k, verbose=1)
 
     return results
+
+def cross_val_test(df, k):
+    results_dict = {}
+    results_df = pd.DataFrame()
+    x = df['text'].values.astype('unicode')
+    y = df['label'].astype(int).tolist()
+    classifiers = {"SVC": LinearSVC(),"DecisionTree": DecisionTreeClassifier(criterion='gini'),"Regression": LogisticRegression(solver='lbfgs')}
+    for key in classifiers.keys():
+        pipeline = Pipeline([
+            ('tfidf_vec', TfidfVectorizer(ngram_range=(2,3))),
+            ('clf', classifiers[key]),
+        ])
+        scores = cross_val_score(pipeline, x, y, cv = k, scoring='f1_micro', error_score="raise")
+        results_dict[str(key)] = scores
+        results_df[str(key)+ " score"] = results_dict[str(key)]
+    return results_df
 
 # select k best features als hyperparameter für DT, Reg??
 
