e4768c30c7c90699b692987ffd218f205d43531f
LillyKate <>
2021-10-10 09:31:00 +0200

    new optics

--- dashboard.py
+++ dashboard.py
@@ -4,8 +4,8 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import base64
-from ml import cross_validate, cross_val_dtree, cross_val_reg, cross_val_mat_SVM
-from most_common_ngrams import most_freq_n_grams
+from ml import cross_validate, cross_val_dtree, cross_val_reg, cross_val_mat_SVM, cross_val_test
+from most_common_ngrams import most_freq_n_grams, most_freq_ngrams_CountVec
 
 import nltk
 from nltk.corpus import stopwords
@@ -13,6 +13,8 @@ from nltk.probability import FreqDist
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 #nltk.download()
+
+import plotly.express as px
 
 # set page dimension, title and icon
 st.set_page_config("Scoring Dashboard", None, "wide", "auto")
@@ -77,7 +79,7 @@ if uploaded_file:
                 ind_label = first_row.index("label")
                 col_label = st.selectbox("Select label column", col_choices, index=ind_label+1)
             else:
-                col_label = st.selectbox("Select/confirm label column", col_choices)
+                col_label = st.selectbox("Select label column", col_choices)
 
             submitted01 = st.form_submit_button('Submit')
             #if submitted01:
@@ -231,45 +233,53 @@ if uploaded_file:
 
 if ndf is not None:
 
+    zero_values = ndf[ndf['id'].isna()|ndf['text'].isna()|ndf['label'].isna()]
     ndf = ndf.dropna(subset=['text'])
 
     with data_stats:
         st.markdown('## **Dataset Statistics**')
-        c0, c01,c02,c_space = st.columns((1,1,1,2))
+        instances = len(ndf)
+        c0, c01,c02,c_space = st.columns((1,1,1.5,1.5))
+
         with c0:
-            instances = len(ndf)
-            st.write('Number of instances: ', str(instances))
-            if instances < 10:
-                st.error("The amount of data is too low for correct analysis.")
-                st.error("Programm will be terminated. Please try again with more data.")
-                st.stop
-                # 'st.stop' stops further processing
-                # noch einen Regulator einfügen? > 'stop'
-            elif instances < 100:
-                st.warning("Data amount low. Results likely skewed.")
-            else:
-                st.success("Data amount sufficient.")
+            st.metric(label = 'Number of instances: ', value=instances, delta=str(len(zero_values)*(-1))+" *")
+            st.markdown("##### * Number of NaN instances")
+            st.write(" ")
+
+        if instances < 10:
+            st.error("The amount of data is too low for correct analysis.")
+            st.error("Programm will be terminated. Please try again with a larger data set.")
+            st.stop()
+            # 'st.stop' stops further processing
+            # noch einen Regulator einfügen? > 'stop'
+        elif instances < 100:
+            st.warning("Data amount low. Results might be skewed.")
+        else:
+            st.success("Data amount is sufficient.")
             
         #@TODO Warning if too low
-
             labels = set(ndf["label"])
-        # label ggf. sorten vor Anzeige?
-            st.write("Number of labels: ", str(len(labels)),"\n",)
-            st.write("Labels: ", str(labels))
             
-        with c01:
-            label_counts = ndf["label"].value_counts()
-            label_freq = ndf["label"].value_counts(normalize=True)
-            st.write("Frequency of labels: ",label_freq)
+        with c01:    
+            st.metric(label = "Number of labels: ", value=len(labels))
         with c02:
-            label_perc = label_freq * 100
-            st.write("Percentage of labels: ",label_perc)
+            show_labels = str(labels)
+            st.metric("Labels: ", show_labels )
         
+        label_counts = ndf["label"].value_counts()
+        label_freq = ndf["label"].value_counts(normalize=True)
+          
         if len(labels) >= 9:
             num_labels = True
         else:
             num_labels = False
+        st.markdown("### Label distribution")
         st.bar_chart(label_freq,width=110*len(labels), use_container_width=num_labels)
+        st.info("*Hover over bars to see exact values")
+
+    
+        #fig3 = sns.catplot(x='label', kind='count',height=5, aspect=3, data=ndf)#,points='all')
+        #st.pyplot(fig3)
         
         # TODO Warning if imbalanced
         # Darstellung der Label Verteilung: Bar Chart mit 110pixel*len(labels), Knackpunkt: 9 Daten-Kolumnen
@@ -278,7 +288,7 @@ if ndf is not None:
         #label_dist = label_counts.min()/label_counts.max()
         #st.write(label_dist)
         label_dist = label_freq.min()/label_freq.max()
-        st.write(label_dist)
+        #st.write(label_dist)
         if label_dist >= 0.9:
             st.success("The labels are evenly distributed.")
         elif label_dist > 0.8:
@@ -291,61 +301,111 @@ if ndf is not None:
         # >> Headings, Barriere-Freiheit, Screen-Reader
 
         # Average Length mit Plot
-        st.subheader("Average length data")
+        st.markdown("### Average length statistics")
         # Achtung: bug in streamlit DataFrame Anzeigebreite issue#371
-        c1, c2 = st.columns((1,2))
-        av_df = pd.DataFrame()
-        st.write("\n")
-        with c1:
-            st.write("Data")
-            st.dataframe(ndf['text'])
-        # average no tokens
-        #av_df['test'] = ndf['text'].apply(lambda x: "Hallo."+ str(x))    # debug Test
-        av_df['#chars/entry'] = ndf['text'].apply(lambda x: len(''.join(str(x).split())))
-        av_df['#words/entry'] = ndf['text'].apply(lambda x: len(str(x).split()))
-        #av_df['#sentences/entry'] = ndf['text'].apply(lambda x: len(str(x).split('.')))
-        av_df['#chars/sentence'] = ndf['text'].apply(lambda x: len(''.join(str(x).split()))/len(str(x).split('.')))
-        av_df['#words/sentence'] = ndf['text'].apply(lambda x: len(str(x).split())/len(str(x).split('.')))
-        av_df['label'] = ndf['label']
-        #av_df['#chars/word'] = ndf['text'].apply(lambda x: len(''.join(str(x).split()))/len(str(x).split()))
-        
-        # vocabulary = set(text.split())
-        
-        #@TODO text in die Tabelle hinzu, scrollbar seitlich? done: gelöst als 2 df nebeneinander
-        #@TODO means + plot
-        with c2:
-            st.write("Average stats: counts + lengths")
-            st.write(av_df)
+        with st.expander("Show single data statistics"):
+            c1, c2 = st.columns((1,2))
+            av_df = pd.DataFrame()
+            st.write("\n")
+            with c1:
+                st.write("Original data")
+                st.dataframe(ndf['text'])
+            # average no tokens
+            #av_df['test'] = ndf['text'].apply(lambda x: "Hallo."+ str(x))    # debug Test
+            av_df['#chars/entry'] = ndf['text'].apply(lambda x: len(''.join(str(x).split())))
+            av_df['#words/entry'] = ndf['text'].apply(lambda x: len(str(x).split()))
+            #av_df['#sentences/entry'] = ndf['text'].apply(lambda x: len(str(x).split('.')))
+            av_df['#chars/sentence'] = ndf['text'].apply(lambda x: len(''.join(str(x).split()))/len(str(x).split('.')))
+            av_df['#words/sentence'] = ndf['text'].apply(lambda x: len(str(x).split())/len(str(x).split('.')))
+            av_df['label'] = ndf['label']
+            #av_df['#chars/word'] = ndf['text'].apply(lambda x: len(''.join(str(x).split()))/len(str(x).split()))
+            
+            # vocabulary = set(text.split())
+            
+            #@TODO text in die Tabelle hinzu, scrollbar seitlich? done: gelöst als 2 df nebeneinander
+            #@TODO means + plot
+            with c2:
+                st.write("Average stats: counts + lengths")
+                st.write(av_df)
 
-        st.line_chart(av_df) # als Scatterplot?
-        #st.bar_chart(av_df)
+        #TODO automated plotting for x labels..
+
+        st.markdown('### Plot Test SNS displot')
+        with st.expander(label="Show/Hide Distribution plots",expanded=True):
+            fig1a = sns.displot(av_df, x="#chars/entry", hue='label', kde = True, height=5, aspect=3)
+            fig1a.set_xlabels("Counts - #chars/entry")
+            fig1a.set_ylabels("Instances")
+            st.pyplot(fig1a)
+
+            fig1b = sns.displot(av_df, x="#chars/sentence", hue='label', kde = True, height=5, aspect=3)
+            fig1b.set_xlabels("Counts - #chars/sentence")
+            fig1b.set_ylabels("Instances")
+            st.pyplot(fig1b)
+
+            fig1c = sns.displot(av_df, x="#words/entry", hue='label', kde = True, height=5, aspect=3)
+            fig1c.set_xlabels("Counts - #words/entry")
+            fig1c.set_ylabels("Instances")
+            st.pyplot(fig1c)
+
+            fig1d = sns.displot(av_df, x="#words/sentence", hue='label', kde = True, height=5, aspect=3)
+            fig1d.set_xlabels("Counts - #words/sentence")
+            fig1d.set_ylabels("Instances")
+            st.pyplot(fig1d)
+
+        st.markdown('### Plot Test - Plotly Boxplot')
+
+        av_df_char = pd.DataFrame()
+        av_df_word = pd.DataFrame()
+        av_df_char['#chars/entry'] = av_df['#chars/entry']
+        av_df_char['#chars/sentence'] = av_df['#chars/sentence']
+        av_df_char['label'] = av_df['label']
+        av_df_word['#words/entry'] = av_df['#words/entry']
+        av_df_word['#words/sentence'] = av_df['#words/sentence']
+        av_df_word['label'] = av_df['label']
+
+        #fig0a = px.box(av_df, color='label')#,points='all')
+        #fig0a.update_layout()#(width=400, height=500)
+        #st.plotly_chart(fig0a)
+
+        word_c, char_c = st.columns(2)
+        with char_c:
+            fig0b = px.box(av_df_char, color='label', color_discrete_sequence=px.colors.qualitative.Vivid)#,points='all')
+            fig0b.update_layout(width=600, 
+                                height=500,
+                                xaxis_title="",
+                                yaxis_title="Counts")
+            st.plotly_chart(fig0b)
+        with word_c:
+            fig0c = px.box(av_df_word, color='label',color_discrete_sequence=px.colors.qualitative.Vivid)#,points='all')
+            fig0c.update_layout(width=600, 
+                                height=500,
+                                xaxis_title="",
+                                yaxis_title="Counts")
+            st.plotly_chart(fig0c)
+        st.info("*hover over plot to see more detailed stats")
         
+        #TODO check out plotly distplot self build...
+    
         st.subheader("Average values on whole data set selection")
         df_mean = av_df.mean()
         df_mean = df_mean.to_frame(name="average value")
-        
-        c3, c4 = st.columns((2,1))
+
+        c1,c2,c3,c4 = st.columns(4)
+        with c1:
+            st.metric(label = 'Average #chars/entry: ', value=round(df_mean.iat[0,0],4))
+        with c2:
+            st.metric(label = 'Average #chars/sentence: ', value=round(df_mean.iat[2,0],4))
         with c3:
-            st.write("\n")
-            st.write(df_mean)
+            st.metric(label = 'Average #words/entry: ', value=round(df_mean.iat[1,0],4))
+        with c4:
+            st.metric(label = 'Average #words/sentence: ', value=round(df_mean.iat[3,0],4))
+        
 
-            df_group_mean = av_df.groupby('label').mean()
-            st.write("Grouped by label",df_group_mean)
-            # add plot - done
-            fig = plt.figure(figsize=(6,2))
-            ax = plt.axes()
-
-            plt.plot(df_group_mean.iloc[0], label='0')
-            plt.plot(df_group_mean.iloc[1], label='1')
-            plt.xlabel('stats')
-            plt.ylabel('frequency')
-            plt.legend(('0','1'))
-            #plt.plot(x,y,color='blue')
-            st.pyplot(fig)
-
-            #@TODO try als bar chart mit bars für jedes label side by side
-
-            st.bar_chart(df_group_mean)
+        df_group_mean = av_df.groupby('label').mean()
+        st.markdown("### Average values grouped by label")
+        st.write(df_group_mean)
+        #TODO build a nice table for display!
+        #Veto against metric display: works good for two labels but not necessarily for more..
 
         st.header("Most frequent word n-grams")
         text_content = ""
@@ -357,8 +417,14 @@ if ndf is not None:
         fdist = FreqDist(text_content)
         
         N = st.slider("Number of most common n-grams to display", value=20)  # get by user input?
-        some_data = most_freq_n_grams(ndf, labels, N)
+        ### Function based on nltk ngrams
+        #some_data = most_freq_n_grams(ndf, labels, N)
         #st.write(some_data)
+        ### Function based on ContVectorizer algorithm
+        # last two parameters: start/stop of n-gram range fpr CountVectorizer
+        n_gram_start = 1
+        n_gram_stop = 3
+        some_more_data = most_freq_ngrams_CountVec(ndf, labels, N, n_gram_start, n_gram_stop)
 
         # Type-Token Ratio
         st.subheader("Type-Token-Ratio")
@@ -375,15 +441,14 @@ if ndf is not None:
 
         with c6:
             st.write("\n")
-            st.bar_chart(ttr_group_mean, width=len(labels)*115, use_container_width=False)
-
-        with c7:
-            st.write("\n")
             st.write("\n")
             st.write("\n")
             st.write("\n")
             st.write(ttr_group_mean)
-            st.write("*left-most column: labels")
+            st.info("*left-most column: labels")
+            
+        st.write("\n")
+        st.bar_chart(ttr_group_mean, width=len(labels)*115, use_container_width=num_labels)
 
         #st.line_chart(ttr_df['TTR'])
         #st.bar_chart(ttr_df['TTR'])
@@ -398,16 +463,17 @@ if ndf is not None:
     labels = st.sidebar.radio(
         "Label type?",
         ('Categorical', 'Numeric - discrete', 'Numeric continuous'))
+        # categorical: String, dicrete: int, continuous: float
 
     #Algorithm?
     algorithm = st.sidebar.radio(
         "Algorithm?",
         ('SVM', 'Regression', 'Decision Tree'))
     
-    
+
     # ML -------------------------------------------------------------------------------------
     st.markdown('# Machine Learning Stats')
-    st.subheader('Chosen algorithm: '+str(algorithm))
+    st.markdown('## Classifier Performance Preview')
 
     #stops_en = set(stopwords.words('english'))
     #stops_de = set(stopwords.words('german'))
@@ -424,6 +490,52 @@ if ndf is not None:
 
     # cv value fest an Funktion übergeben oder als variablen Input vom User?
     
+    overview_res = cross_val_test(ndf, 10) # 
+    ## moved following lines to function, so it returns a data frame
+    #overview_scores = pd.DataFrame()
+    #for key in overview_res.keys():
+    #    overview_scores[str(key)+ " score"] = overview_res[key]
+    overview_res_ = overview_res.reset_index().rename(columns={'index': 'fold', 'score': 'score'})
+    overview_res_['fold']=overview_res_['fold'].values + 1
+    overview_stats = [] # columns require multiple iterations, calculate only once
+    for elem in overview_res.columns:
+        overview_stats.append((str(elem[:len(elem)-6]),(round(overview_res_[elem].mean(),4)),(round(overview_res_[elem].std(),4))))
+    
+    c_name, c_score, c_std, c_plot = st.columns((0.7,0.7,0.7,3))
+    with c_name:
+        max_score = overview_stats[0][1]
+        # also for std? max std and trends?
+        for (name, mean,std) in overview_stats:
+            st.write(" ")
+            st.write(name)
+            st.write(" ")
+            st.write(" ")
+            st.write(" ")
+            if mean >= max_score:
+                max_score = mean
+
+    with c_score:
+        for (name, mean,std) in overview_stats:
+            if mean == max_score:
+                st.metric(label = 'Mean Score: ', value=mean)
+            else:
+                st.metric(label = 'Mean Score: ', value=mean, delta=round(mean-max_score,2))
+
+    with c_std:
+        for (name, mean,std) in overview_stats:
+            st.metric(label = 'Mean Std: ', value=std)
+    with c_plot:
+        fig4a = px.box(overview_res, points='all')
+        #fig4a.update_yaxes(range=[0.6,1.0])
+        fig4a.update_layout(height = 450,
+                            width=600, 
+                            xaxis_title="Classifier",
+                            yaxis_title="Score",
+                            margin=dict(l=20,r=20,t=20,b=10))
+            
+        st.plotly_chart(fig4a)
+        st.info("*hover over graphs for more details")
+
     if algorithm == 'SVM':
         results = cross_val_mat_SVM(ndf, 10)
     elif algorithm == 'Regression':
@@ -433,47 +545,53 @@ if ndf is not None:
 
     scores = results['scores']
 
-    x = range(0,len(scores))
-    y = scores
     df_scores = pd.DataFrame(data=scores, columns=['score'])
-    c7a, c7, c8 = st.columns((0.25,0.75,3.5))
-    with c7:
-        st.write("\n")
+    scores_ = df_scores.reset_index().rename(columns={'index': 'fold', 'score': 'score'})
+    scores_['fold']=scores_['fold'].values + 1
+    
+    st.markdown('## Chosen Classifier Performance')
+    c7b, c7c, c8 = st.columns((1.15,0.9, 3))
+    with c7b:
+        st.metric(label="", value="")
+        st.metric(label='Chosen algorithm: ',value = str(algorithm))
+        st.metric(label = 'Mean Score: ', value=round(scores.mean(),4))#, delta=(len(zero_values)*(-1)))
+        st.metric(label = 'Mean Std: ', value=round(scores.std(),4))
+    with c7c:
+        st.metric(label="", value="")
         st.write(df_scores) # wollen wir das ganz anzeigen? Oder nur den mean + std deviation?
+    
     with c8:
-
     # Gesamtgröße auf Seite anpassen
     # > requires Work-around, streamlit always scales to max container width..
-        fig = plt.figure(figsize=(7,2))
-        ax = plt.axes()
-        ax.set_xlim(0,len(scores)-1)
-        ax.set_ylim(0,1)
-        plt.xlabel('CV fold')
-        plt.ylabel('Score')
-        plt.plot(x,y,color='orange')
-        st.pyplot(fig)
+        #fig4b = px.box(scores_,x='fold', y='score', points='all')
+        fig4b = px.box(scores, points='all')
+        #fig4b.update_yaxes(range=[0.6,1.0])
+        fig4b.update_layout(height = 450,
+                            width=250, 
+                            xaxis_title=" ",
+                            yaxis_title="Score",
+                            margin=dict(l=20,r=20,t=20,b=10))
+            
+        st.plotly_chart(fig4b)
 
-    st.markdown("#### Mean Scores: "+ str(scores.mean()))
-    st.markdown("#### Mean Standard deviation: "+ str(scores.std()))
-
-
-    c9,c10 = st.columns((6,7))
-    with c9:
-        st.markdown('## Confusion matrix')
+    c9a, c9b,c10 = st.columns((0.25,6.5,6.5))
+    with c9b:
+        st.markdown('## **Confusion matrix**')
         conf_mat = confusion_matrix(results['y'],results['y_pred'])
-        fig1 = plt.figure(figsize=(5,4))
+        fig1 = plt.figure(figsize=(4,3))
         sns.heatmap(conf_mat, annot=True, cmap=plt.cm.Blues)
         plt.tight_layout()
         plt.ylabel('Gold label')
         plt.xlabel('Predicted label')
         st.pyplot(fig1)
   
-    with c10:
-        st.markdown('## Classification report')
+    #with c10:
+        st.markdown('## **Classification report**')
         stats = classification_report(results['y'],results['y_pred'], output_dict=True)
-        fig2 = plt.figure(figsize=(5,4))
+        fig2 = plt.figure(figsize=(5.4,4))
         # sns.heatmap(pd.DataFrame(stats).iloc[:-1,:].T, annot=True, cmap=plt.cm.Blues) # to exclude support
         # standard cmap "Blues", yellow-green-purple alternative: "viridism" red-blue: "coolwarm", red-black-ish: "magma"
         # dezent bunt: "cubehelix", "Spectral"
         sns.heatmap(pd.DataFrame(stats).iloc[:-1,:].T, annot=True, cmap=plt.cm.coolwarm)
         st.pyplot(fig2)
+
